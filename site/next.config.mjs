1c56cb31f979bede66bde724910902d43ed98b04
hantang <34468720+hantang@users.noreply.github.com>
2024-03-11 17:58:16 +0800

    Support static export and disable contributor list

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV === 'production';
-const isProd = true;
+const isProd = process.env.NODE_ENV === 'production';
+// const isProd = true;
 
 const nextConfig = {
     basePath: isProd ? '/wayback-gitmoji' : '',
     assetPrefix: isProd ? '/wayback-gitmoji/' : '',  
+    output: 'export',
 };
 
 export default nextConfig;
--- site/src/pages/contributors.tsx
+++ site/src/pages/contributors.tsx
@@ -11,48 +11,48 @@ type Contributor = {
 }
 
 const Contributors = (
-  props: InferGetStaticPropsType<typeof getStaticProps>,
+  // props: InferGetStaticPropsType<typeof getStaticProps>,
 ) => (
   <>
-    <SEO pageTitle="Contributors" pageUrl="/contributors" />
+    {/* <SEO pageTitle="Contributors" pageUrl="/contributors" /> */}
 
     <main>
       <CarbonAd />
       <section>
         <h1>Contributors</h1>
 
-        <ContributorsList contributors={props.contributors} />
+        {/* <ContributorsList contributors={props.contributors} /> */}
       </section>
     </main>
   </>
 )
 
-export const getStaticProps: GetStaticProps<{
-  contributors: Contributor[]
-}> = async () => {
-  type GitHubContributor = {
-    avatar_url: string
-    id: string
-    html_url: string
-    login: string
-  }
-  const response = await fetch(
-    'https://api.github.com/repos/carloscuesta/gitmoji/contributors',
-  )
-  const contributors: GitHubContributor[] = await response.json()
+// export const getStaticProps: GetStaticProps<{
+//   contributors: Contributor[]
+// }> = async () => {
+//   type GitHubContributor = {
+//     avatar_url: string
+//     id: string
+//     html_url: string
+//     login: string
+//   }
+//   const response = await fetch(
+//     'https://api.github.com/repos/carloscuesta/gitmoji/contributors',
+//   )
+//   const contributors: GitHubContributor[] = await response.json()
 
-  return {
-    props: {
-      contributors: contributors
-        .filter((contributor) => !contributor.login.includes('bot'))
-        .map((contributor) => ({
-          avatar: contributor.avatar_url,
-          id: contributor.id,
-          url: contributor.html_url,
-        })),
-    },
-    revalidate: 3600 * 3,
-  }
-}
+//   return {
+//     props: {
+//       contributors: contributors
+//         .filter((contributor) => !contributor.login.includes('bot'))
+//         .map((contributor) => ({
+//           avatar: contributor.avatar_url,
+//           id: contributor.id,
+//           url: contributor.html_url,
+//         })),
+//     },
+//     revalidate: 3600 * 3,
+//   }
+// }
 
 export default Contributors
