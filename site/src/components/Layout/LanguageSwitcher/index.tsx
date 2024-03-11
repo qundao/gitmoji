11ccd29e9c6f3fa5900e52bc2664e9b433dceab7
hantang <34468720+hantang@users.noreply.github.com>
2024-03-11 16:00:19 +0800

    Update language switch

--- site/src/components/Layout/LanguageSwitcher/index.tsx
+++ site/src/components/Layout/LanguageSwitcher/index.tsx
@@ -1,14 +1,29 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import { useLanguage } from 'src/contexts/LanguageContext';
+import styles from './styles.module.css'
+import LangIcon from './LangIcon'
 
 const LanguageSwitcher = () => {
-  const { switchLanguage } = useLanguage();
+  const { lang, switchLanguage } = useLanguage();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleChangeLanguage = (event) => {
+    switchLanguage(event.target.value);
+    setIsOpen(false); 
+  };
 
   return (
-    <div>
-      <button onClick={() => switchLanguage('en')}>Switch to English</button>
-      <button onClick={() => switchLanguage('zh')}>切换到中文</button>
+    <div className={styles.languageSwitcher}>
+      <div className={styles.languageToggle} onClick={() => setIsOpen(!isOpen)}>
+      <LangIcon /><span>{lang === 'en' ? 'English' : '中文'} </span>
+      </div>
+      {isOpen && (
+      <select value={lang} onChange={handleChangeLanguage} className="language-select" size={2}>
+        <option value="en">English</option>
+        <option value="zh">中文</option>
+      </select>
+      )}
     </div>
   );
 };
