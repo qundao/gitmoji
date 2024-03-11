f0da0ca5c9daaa509e804157062966bc0c2b4a9d
hantang <34468720+hantang@users.noreply.github.com>
2024-03-11 13:29:09 +0800

    Set url

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const isProd = process.env.NODE_ENV === 'production';
+const isProd = true;
+
+const nextConfig = {
+    basePath: isProd ? '/wayback-gitmoji' : '',
+    assetPrefix: isProd ? '/wayback-gitmoji/' : '',  
+};
 
 export default nextConfig;
